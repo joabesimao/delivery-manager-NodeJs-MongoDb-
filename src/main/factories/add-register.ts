9f00423f14c14a6239ca62fbd0dfb230562e7e50
joabesimao <simaojoabecosta@gmail.com>
2025-05-31 19:02:49 -0300

    refactor: remove validations unused

--- src/main/factories/add-register.ts
+++ src/main/factories/add-register.ts
@@ -2,21 +2,12 @@ import { AddRegisterController } from "../../presentation/controllers/register-c
 import { DbAddRegister } from "../../data/usescases/register-usecases/add-register/db-add-register";
 import { Controller } from "../../presentation/protocols/controller";
 import { makeAddRegisterValidation } from "./add-register-validation";
-import { makeAddClientValidation } from "./add-client-validation";
-import { makeAddAddressValidation } from "./add-address-validation";
 import { RegisterMongoRepository } from "../../infra/db/mongodb/register-repository/register";
 
 export const makeAddRegisterController = (): Controller => {
   const registerRepository = new RegisterMongoRepository();
   const addRegister = new DbAddRegister(registerRepository);
   const validation = makeAddRegisterValidation();
-  const validationClient = makeAddClientValidation();
-  const validationAddress = makeAddAddressValidation();
-  const registerController = new AddRegisterController(
-    addRegister,
-    validation,
-    validationClient,
-    validationAddress
-  );
+  const registerController = new AddRegisterController(addRegister);
   return registerController;
 };
