7f85182c5e5c78692a56dbc43b123087b38cd9dc
joabesimao <simaojoabecosta@gmail.com>
2025-05-31 19:03:04 -0300

    refactor: remove validations unused

--- src/presentation/controllers/register-controllers/add-register/add-register.ts
+++ src/presentation/controllers/register-controllers/add-register/add-register.ts
@@ -5,35 +5,10 @@ import { HttpRequest, HttpResponse } from "../../../protocols/http";
 import { Validation } from "../../../protocols/validation";
 
 export class AddRegisterController implements Controller {
-  constructor(
-    private readonly addRegister: AddRegister,
-    private readonly validationBody: Validation,
-    private readonly validationClient: Validation,
-    private readonly validationAddress: Validation
-  ) {}
+  constructor(private readonly addRegister: AddRegister) {}
   async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
     try {
       const { client, address } = httpRequest.body;
-
-      const errorBody = await this.validationBody.validate(httpRequest.body);
-      if (errorBody) {
-        return badRequest(errorBody);
-      }
-
-      const errorBodyClient = await this.validationClient.validate(
-        httpRequest.body.client
-      );
-      if (errorBodyClient) {
-        return badRequest(errorBodyClient);
-      }
-
-      const errorBodyAddress = await this.validationAddress.validate(
-        httpRequest.body.address
-      );
-
-      if (errorBodyAddress) {
-        return badRequest(errorBodyAddress);
-      }
 
       const result = await this.addRegister.add({
         client: client,
